712313587be9e64db63721e10baf83f2b4feaab6
Henry Spanka - myVirtualserver <henry@myvirtualserver.de>
2015-11-20 17:54:08 +0100

    fix missing dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-APT_DEPS="openssl exim4 libcrypt-ssleay-perl"
-PERL_DEPS="Data::Validate::IP YAML:XS Email::MIME Email::Sender::Simple LWP::UserAgent JSON"
+APT_DEPS="openssl exim4 libcrypt-ssleay-perl libssl-dev ca-certificates"
+PERL_DEPS="Data::Validate::IP YAML:XS Email::MIME Email::Sender::Simple LWP::UserAgent IO::Socket::SSL Net::SSLeay LWP::Protocol::https JSON"
 
 apt-get install -y $APT_DEPS
 cpan install $PERL_DEPS
